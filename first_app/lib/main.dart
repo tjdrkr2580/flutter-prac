49612cce327a7e53e33855272d61c003c5fd7613
kimtaehyun <tjdrkr2580@naver.com>
2023-06-17 00:37:52 +0900

    lecture : header 2.0

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -9,13 +9,39 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('hello, flutter!'),
-        elevation: 0,
-      ),
-      body: Center(
-        child: Text('center text'),
-      ),
-    ));
+            backgroundColor: Color(0xFF181818),
+            body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 14),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: 80,
+                          ),
+                          Text(
+                            "Hey, Selena",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w800),
+                          ),
+                          Text(
+                            "Welcome, Back",
+                            style: TextStyle(
+                                color: Colors.white.withOpacity(0.75),
+                                fontSize: 18),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )));
   }
 }
