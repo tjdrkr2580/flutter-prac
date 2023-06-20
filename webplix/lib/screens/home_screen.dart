3550a6582b7fe2e8605f34ae475ebc21e0540c17
kimtaehyun <tjdrkr2580@naver.com>
2023-06-21 00:01:16 +0900

    lecture : detail screen

--- webplix/lib/screens/home_screen.dart
+++ webplix/lib/screens/home_screen.dart
@@ -22,17 +22,12 @@ class HomeScreen extends StatelessWidget {
       body: FutureBuilder(
         builder: (context, snapshot) {
           if (snapshot.hasData == true) {
-            return ListView.separated(
-              scrollDirection: Axis.vertical,
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                var toon = snapshot.data![index];
-                return Text(toon.title);
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 30);
-              },
-            );
+            return Column(children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Expanded(child: makeList(snapshot))
+            ]);
           }
           return const Center(
             child: CircularProgressIndicator(),
@@ -40,6 +35,49 @@ class HomeScreen extends StatelessWidget {
         },
         future: toons,
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var toon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.3),
+                  )
+                ],
+              ),
+              child: Image.network(
+                toon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              toon.title,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            )
+          ],
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(width: 30);
+      },
     );
   }
 }
